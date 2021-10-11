15f10881e645105d93f430a59175650e16d3fe95
sozvic <sozvic!@gmail.com>
2021-10-11 15:18:57 +0300

    Add project files.

---
+++ CRUD/Animals.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyCrud
+{
+    public class Animals
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Breed { get; set; }      
+
+    }
+}
---
+++ CRUD/Controllers/AnimalsController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCrud.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AnimalsController : ControllerBase
+    {
+        private static List<Animals> _animals = new List<Animals>()
+        {
+          new Animals() { Id = 1, Age = 5, Name = "Bob", Breed = "Buldog" },
+          new Animals() { Id = 2, Age = 3, Name = "Cesar", Breed = "Taksa" },
+          new Animals() { Id = 3, Age = 7, Name = "Neptun", Breed = "Rassel" },
+          new Animals() { Id = 4, Age = 1, Name = "Charli", Breed = "RedRiver" }
+         
+        };
+
+        private readonly ILogger<AnimalsController> _logger;
+
+        public AnimalsController(ILogger<AnimalsController> logger)
+        {
+     
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(_animals);
+              
+        }
+        [HttpPost]
+        public IActionResult Post(Animals animals)
+        {
+            animals.Id = 5;
+            animals.Name = "Vasya";
+            animals.Age = 10;
+            animals.Breed = "Labrodor";
+            _animals.Add(animals);
+
+
+            return Created("5", animals);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Animals animals)
+        {
+            var value = _animals.Where(x => x.Id == id).FirstOrDefault();
+            if (value != null)
+            {
+                foreach (var item in _animals)
+                {
+                    if (item.Id == id)
+                    {
+                        item.Breed = "Chappy";
+                    }  
+                }
+
+                animals.Breed = "Chappy";
+                return Ok(animals);
+              
+            }
+            return NotFound();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+           var value = _animals.Where(x => x.Id == id).FirstOrDefault();
+            if (value != null) 
+                return Ok(value);
+            else return NotFound();
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var value = _animals.Where(x => x.Id == id).FirstOrDefault();
+            if (value != null)
+            {
+                _animals.Remove(value);
+                return Ok(value);
+            }
+            return NotFound();
+
+        }
+    }
+}
